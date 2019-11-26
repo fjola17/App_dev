0448626beb978a2d97d096f2896ac43768f6686d
fjola17 <fjola17@ru.is>
2019-11-26 18:17:27 +0000

    fix

--- Toddler/src/components/Board/BoardInput/index.jsx
+++ Toddler/src/components/Board/BoardInput/index.jsx
@@ -9,13 +9,10 @@ class BoardInput extends React.Component{
         nameError: ''
     }
     
-    updateName(name, value){
-        console.log(name, value, this.state)
-        this.setState({[name]:value})
-    }
-    validate(){
+    validate(){ 
         const name = this.state.name;
-        let nameError = this.state.nameError;
+        let nameError = '';
+        console.log(name)
         if(!name){
             nameError = "Name for the board is required"
         }
@@ -40,7 +37,7 @@ class BoardInput extends React.Component{
         }
         return(
             <View>
-                <TextInput placeholder="Please enter a name for your board" value={this.state.name} onChangeText={ (text) => this.updateName("name",this.text) } />
+                <TextInput placeholder="Please enter a name for your board" onChangeText={ (value) => this.setState({name:value}) } value={this.state.name} />
                 <Text>{this.state.nameError}</Text>
                 <Button title="Select a image"></Button>
                 <Button disabled={hasErrors} onPress={() => this.validate()} title="Submit"><Text>Sub</Text></Button>
