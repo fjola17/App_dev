ea4547a77c33f963028d5f404f10552831ddbfa5
HlynurM <futomatik@gmail.com>
2019-11-30 22:23:20 +0000

    more button changes

--- Toddler/src/components/TaskInput/styles.js
+++ Toddler/src/components/TaskInput/styles.js
@@ -2,8 +2,15 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
   button: {
-    color: 'red',
-    fontWeight: 'bold',
-    fontSize: 20,
+    fontWeight: 'bold', 
+    fontSize: 16, 
+    color: 'white',
+    paddingTop: 5,
+    paddingBottom: 5,
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    borderColor: 'white',
+    borderWidth: 1,
+    backgroundColor: 'green',
   },
 });
