ed0694a053d019f1425c93f3565ee3d575bdf7e7
fjola17 <fjola17@ru.is>
2019-12-02 16:33:04 +0000

    Basic search works for random strings

--- thecontractor/src/components/SearchBar/index.jsx
+++ thecontractor/src/components/SearchBar/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, TextInput, Text, Button, TouchableOpacity } from 'react-native';
+import { View, TextInput, Text, TouchableOpacity } from 'react-native';
 import styles from './styles';
 
 class SearchBar extends React.Component {
@@ -7,15 +7,24 @@ class SearchBar extends React.Component {
     super(props);
     this.state = {
       input: '',
+      contacts: ["abdfaa", "hello", "I am your father"],
+      // filteredContacts: [],
     };
+  }
+
+  search() {
+    const { input, contacts } = this.state;
+    return contacts.filter((con) => con.toLowerCase().indexOf(input.toLowerCase()) !== -1);
   }
 
   render() {
     const { input } = this.state;
+    const search = this.search();
     return (
       <View style={styles.container}>
         <TextInput placeholder="Search string" onChangeText={(val) => this.setState({ input: val })} value={input} />
         <TouchableOpacity><Text>Search</Text></TouchableOpacity>
+        <Text>{search}</Text>
       </View>
     );
   }
