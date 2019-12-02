4faba6de0987bd69909bdb7b25ee088fd12806f8
HlynurM <futomatik@gmail.com>
2019-12-02 15:22:53 +0000

    Imperial color pallett in colors.js

---
+++ thecontractor/src/Views/Contacts/styles.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from 'react-native';
+// import { mainColor } from '../../styles/colors';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#333',
+    alignItems: 'center',
+  },
+  textHeader: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+    lineHeight: 20,
+  },
+  textTagline: {
+    color: 'red',
+    fontSize: 14,
+    fontWeight: 'normal',
+    lineHeight: 35,
+  },
+  textMainBody: {
+    color: 'white',
+    fontSize: 14,
+    fontWeight: 'normal',
+    lineHeight: 30,
+  },
+});
---
+++ thecontractor/src/styles/colors.js
@@ -0,0 +1,9 @@
+// Empire Colors
+export const impBlack = '#1e1e1e';
+export const impDark = '#2d2d2d';
+export const impLighterDark = '#424242';
+export const impRed = '#ed424d';
+export const impBlasterRed = '#f23207';
+export const impOrange = '#e08e1a';
+export const impWhite = '#f9f9f9';
+// export const impDark = '#333';
