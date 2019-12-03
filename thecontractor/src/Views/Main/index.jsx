b1f70a81d881a929460021d0b5c051a44600b9e3
Arnþór Logi Arnarson <arnthorla@gmail.com>
2019-12-03 15:03:54 +0000

    Got search working. Used SearchBar from react-native-elements run: npm -i react-native-elements --save

--- thecontractor/src/Views/Main/index.jsx
+++ thecontractor/src/Views/Main/index.jsx
@@ -1,27 +1,61 @@
 import React from 'react';
-import { View } from 'react-native';
-import SearchBar from '../../components/SearchBar';
+import { View, FlatList } from 'react-native';
+import { SearchBar } from 'react-native-elements';
 import SmallContact from '../../components/SmallContact';
 import data from '../../resources/contacts';
 
 class Main extends React.Component {
   constructor(props) {
     super(props);
+    const { navigate } = this.props;
     const { contacts } = data;
     this.state = {
-      contacts,
+      navigate,
+      contacts: contacts.sort((a, b) => a.name !== b.name ? (a.name < b.name ? -1 : 1) : 0),
+      search: '',
+      result: [],
     };
   }
 
+  componentDidMount() {
+    this.SearchFilterFunction(''); // To show full list on start
+  }
+
+  SearchFilterFunction(text) {
+    // passing the inserted text in textinput
+    const newData = this.state.contacts.filter((item) => {
+      // applying filter for the inserted text in search bar
+      const itemData = item.name ? item.name.toUpperCase() : ''.toUpperCase();
+      const textData = text.toUpperCase();
+      return itemData.indexOf(textData) > -1;
+    });
+    this.setState({
+      // setting the filtered newData on datasource
+      // After setting the data it will automatically re-render the view
+      result: newData,
+      search: text,
+    });
+  }
+
   render() {
-    const { contacts } = this.state;
-    const result = contacts.map(({ name, image }) => (
-      <SmallContact key={name} name={name} image={image} />
-    ));
+    const { result, search, navigate } = this.state;
     return (
       <View>
-        <SearchBar contacts={contacts} />
-        {result}
+        <SearchBar
+          searchIcon={{ size: 24 }} // Size of the search icon
+          onChangeText={(text) => this.SearchFilterFunction(text)}
+          // Filter the list using the keywords inserted in searchbar
+          onClear={(text) => this.SearchFilterFunction('')}
+          placeholder="Type Here..."
+          value={search}
+        />
+        <FlatList
+          data={result}
+          renderItem={({ item }) => (
+            <SmallContact contacts={item} />
+          )}
+          keyExtractor={(item) => item.name}
+        />
       </View>
     );
   }
