4c8194f7258572cdf8b7dc332331ffe513448b59
fjola17 <fjola17@ru.is>
2019-12-09 15:27:01 +0000

    basic redux

--- drCinema/App.jsx
+++ drCinema/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import Routes from './src/routes';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider, connect } from 'react-redux';
+import reducers from './src/reducers';
+import thunk from 'redux-thunk';
+
+class SubApp extends React.Component {
+  componentDidMount() {
+
+  }
+
+  render(){
+    return (
+      <Routes />
+    );
+  }
+}
+
+const ConnectedSubApp = connect(null, {})(SubApp);
 
 export default function App() {
   return (
-    <Routes />
+    <Provider store={createStore(reducers, applyMiddleware(thunk))}>
+      <ConnectedSubApp />
+    </Provider>
   );
 }
---
+++ drCinema/src/reducers/cinema.js
@@ -0,0 +1,7 @@
+// displays the movie theater
+
+export default function(state, action){
+    switch (action.type){
+    default: return null;
+    }
+}
